3751a010538e31d8e099d7c21ebc2cdb086fd70f
crist <c.gallegos09@ufromail.cl>
2025-05-15 19:17:53 -0400

    Pom actualizado junto con clases de FarmEasyApp para poder visualizar la base de datos y buscar medicamentos

--- src/main/java/org/example/Farmacia.java
+++ src/main/java/org/example/Farmacia.java
@@ -4,46 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Farmacia {
-    private String idFarmacia;
     private String nombre;
-    private Ubicacion ubicacion;
-    private List<Medicamento> listaMedicamentos = new ArrayList<>(); // ✅ INICIALIZADA
-    private List<PrecioFarmacia> precios = new ArrayList<>();
+    private List<Medicamento> listaMedicamentos = new ArrayList<>();
 
-    public Farmacia(String nombre, Ubicacion ubicacion){
+    public Farmacia(String nombre) {
         this.nombre = nombre;
-        this.ubicacion = ubicacion;
-    }
-
-    public void obtenerMedicamentos(){
-
-        System.out.println("Obteniendo medicamentos de la página web de la farmacia: " + nombre);
-
-        // Simulamos que se extrajeron estos medicamentos
-        listaMedicamentos.add(new Medicamento("Paracetamol", "500mg"));
-        listaMedicamentos.add(new Medicamento("Ibuprofeno", "400mg"));
-        listaMedicamentos.add(new Medicamento("Amoxicilina", "500mg"));
-
-        System.out.println("Medicamentos obtenidos correctamente.");
-    }
-
-    public List<Medicamento> getListaMedicamentos() {
-        return listaMedicamentos;
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public Ubicacion getUbicacion() {
-        return ubicacion;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
-    public List<PrecioFarmacia> getPrecios() {
-        return precios;
+    public List<Medicamento> getListaMedicamentos() {
+        return listaMedicamentos;
     }
 
-    public void agregarPrecio(PrecioFarmacia precio) {
-        precios.add(precio);
+    public void agregarMedicamento(Medicamento medicamento) {
+        listaMedicamentos.add(medicamento);
     }
 }
