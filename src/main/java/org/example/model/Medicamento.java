68a6667d8598b878f6cdef2732361fe5a8dc6999
ProyectoAutos <c.gallegos09@ufromail.cl>
2025-05-30 17:58:20 -0400

    new brand

---
+++ src/main/java/org/example/model/Medicamento.java
@@ -0,0 +1,11 @@
+package org.example.model;
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "medicamentos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Medicamento {
+}
